e5eac5d19f04da89567e146c9efe9b7c9ea56df5
xiauhna <xiauhna@outlook.com>
2023-02-08 00:49:07 +0800

    leiue查询

--- getIPLocation.py
+++ getIPLocation.py
@@ -1,15 +1,21 @@
 import json
 import requests
+import traceback
 
-def getlei():
-    url = ""
+def getCountry(ip):
+    url = "https://ip.leiue.com/?ip={}&chk=0&id=0".format(ip)
+    # fake-useragent
     headers = {
-        "User-Agent" : ""
+        "User-Agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
     }
-    resp = requests.get(url, headers=headers)
+    try:
+        resp = requests.get(url, headers=headers)
+    except Exception:
+        print(traceback.format_exc())
     if resp.status_code != 200:
-        return
+        return 0
     return json.loads(resp.text)["country"]
 
+
 if __name__ == '__main__':
-    getlei()
+    print(getCountry('8.8.8.8'))
